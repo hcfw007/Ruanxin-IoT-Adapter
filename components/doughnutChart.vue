fb9da8148cc50dc0654cd42abeb91cd48ee6cc9f
hcfw007 <brotherstyx@gmail.com>
2020-10-29 20:07:18 +0800

    feat: install and config chartjs

---
+++ components/doughnutChart.vue
@@ -0,0 +1,27 @@
+<script>
+/* eslint-disable vue/require-prop-types */
+/* eslint-disable vue/require-prop-types */
+import { Doughnut, mixins } from 'vue-chartjs'
+
+const baseOption = {
+  cutoutPercentage: 70,
+  legend: {
+    display: false
+  }
+}
+
+export default {
+  extends: Doughnut,
+  mixins: [mixins.reactiveProp],
+  props: ['customOptions'],
+  data() {
+    return {
+      options: {}
+    }
+  },
+  mounted() {
+    this.options = Object.assign(baseOption, this.customOptions)
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
---
+++ components/lineChart.vue
@@ -0,0 +1,40 @@
+<script>
+/* eslint-disable vue/require-prop-types */
+/* eslint-disable vue/require-prop-types */
+import { Line, mixins } from 'vue-chartjs'
+
+const baseOption = {
+  legend: {
+    display: false
+  },
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    xAxes: [{
+      gridLines: {
+        display: false
+      }
+    }],
+    yAxes: [{
+      gridLines: {
+        display: false
+      }
+    }]
+  }
+}
+
+export default {
+  extends: Line,
+  mixins: [mixins.reactiveProp],
+  props: ['customOptions'],
+  data() {
+    return {
+      options: {}
+    }
+  },
+  mounted() {
+    this.options = Object.assign(baseOption, this.customOptions)
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
