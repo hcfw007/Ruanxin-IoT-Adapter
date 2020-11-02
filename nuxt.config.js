1f059df093c6f567b28f99a8c121bc4a6d0139bb
hcfw007 <brotherstyx@gmail.com>
2020-11-02 14:16:31 +0800

    style: change deprecated item

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,6 @@
 
 export default {
-  /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
-  mode: 'universal',
+  ssr: false,
   /*
   ** Nuxt target
   ** See https://nuxtjs.org/api/configuration-target
