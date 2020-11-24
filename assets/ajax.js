662a9d5b98c55eb98fa417f2ed55c46425e703f9
hcfw007 <brotherstyx@gmail.com>
2020-11-24 16:52:31 +0800

    refactor: unify ajax structure

--- assets/ajax.js
+++ assets/ajax.js
@@ -51,6 +51,7 @@ const putRequestFactory = url => async (vueObj, data = {}, successToastMessage =
 }
 
 const postRequestFactory = url => async (vueObj, data = {}, successToastMessage = '成功', failedToastMessage = '失败', urlReplacementItem = {}) => {
+  let flag = 'origin'
   for (let item in urlReplacementItem) {
     url = url.replace('${' + item + '}', urlReplacementItem[item])
   }
@@ -66,6 +67,7 @@ const postRequestFactory = url => async (vueObj, data = {}, successToastMessage 
           color: '#fff'
         }
       })
+      flag = true
     } else {
       vueObj.$toast(failedToastMessage + '，消息为' + responseData.msg, {
         customCss: {
@@ -73,6 +75,7 @@ const postRequestFactory = url => async (vueObj, data = {}, successToastMessage 
           color: '#fff'
         }
       })
+      flage = false
     }
   }).catch((err) => {
     vueObj.$toast(failedToastMessage + '，消息为' + err.message, {
@@ -81,27 +84,42 @@ const postRequestFactory = url => async (vueObj, data = {}, successToastMessage 
         color: '#fff'
       }
     })
+    flag = false
   })
+  return flag
 }
 
-const vueDataObjectGetterFactory = url => (vueObj, dataItemName, data = {}, urlReplacementItem = {}) => {
+const getRequestFactory = url => async (vueObj, dataItemName, data = {}, urlReplacementItem = {}) => {
+  let flag = 'origin'
   for (let item in urlReplacementItem) {
     url = url.replace('${' + item + '}', urlReplacementItem[item])
   }
-  instance.get(url, { data }).then((response) => {
+  await instance.get(url, { data }).then((response) => {
     let responseData = response.data
     if (typeof responseData === 'string') {
       responseData = JSON.parse(responseData)
     }
     if (responseData.code === 200) {
       vueObj[dataItemName] = responseData.data
+      flag = true
     } else {
       vueObj.$toast(responseData.msg)
+      flag = false
     }
+  }).catch((err) => {
+    vueObj.$toast('无法连接服务器，错误信息为' + err.message + '， 请刷新重试', {
+      customCss: {
+        'background-color': '#E6A23C',
+        color: '#fff'
+      }
+    })
+    flag = false
   })
+  return flag
 }
 
 const deleteRequestFactory = url => async (vueObj, data = {}, successToastMessage = '成功', failedToastMessage = '失败', urlReplacementItem = {}) => {
+  let flag = 'origin'
   for (let item in urlReplacementItem) {
     url = url.replace('${' + item + '}', urlReplacementItem[item])
   }
@@ -117,6 +135,7 @@ const deleteRequestFactory = url => async (vueObj, data = {}, successToastMessag
           color: '#fff'
         }
       })
+      flag = true
     } else {
       vueObj.$toast(failedToastMessage + '，消息为' + responseData.msg, {
         customCss: {
@@ -124,6 +143,7 @@ const deleteRequestFactory = url => async (vueObj, data = {}, successToastMessag
           color: '#fff'
         }
       })
+      flag = false
     }
   }).catch((err) => {
     vueObj.$toast(failedToastMessage + '，消息为' + err.message, {
@@ -132,24 +152,26 @@ const deleteRequestFactory = url => async (vueObj, data = {}, successToastMessag
         color: '#fff'
       }
     })
+    flag = false
   })
+  return flag
 }
 // 获取行业列表
-export const getIndustryList = vueDataObjectGetterFactory('/industry/select')
+export const getIndustryList = getRequestFactory('/industry/select')
 // 获取类型列表
-export const getCategoryList = vueDataObjectGetterFactory('/product_categories')
+export const getCategoryList = getRequestFactory('/product_categories')
 
 // 添加新产品
 export const postNewProduct = postRequestFactory('/products/create')
 // 获取产品列表
-export const getProductList = vueDataObjectGetterFactory('/products/select')
+export const getProductList = getRequestFactory('/products/select')
 
 // 获取产品的功能点
-export const getProductFunctionList = vueDataObjectGetterFactory('/products/${id}/functions')
+export const getProductFunctionList = getRequestFactory('/products/${id}/functions')
 // 获取所有标准功能点
 
 // 功能点添加到产品之后，对于这个combination会产生一个新的id
-export const getFunctionList = vueDataObjectGetterFactory('/functions/standard')
+export const getFunctionList = getRequestFactory('/functions/standard')
 // 保存标准功能点
 export const postProductFunctionList = postRequestFactory('/products/${id}/functions/standard')
 // 保存自定义功能点
